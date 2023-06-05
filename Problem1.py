8c890a90b7c14fdafcd375c94a0e983b7094eea9
Daria-03 <kvzss.rz.07.pa@mail.ru>
2023-06-05 11:21:17 +0600

    first commit

---
+++ Problem1.py
@@ -0,0 +1,14 @@
+n1 = int(input('Введи количество элементов первого набора чисел: '))
+n2 = int(input('Введи количество элементов второго набора чисел: '))
+arr1 = []
+arr2 = []
+for i in range(n1):
+    arr1.append(int(input('Введи элемент первого массива: ')))
+for j in range(n2):
+    arr2.append(int(input('Введи элемент второго массива: ')))
+arr3 = []
+for i in arr1:
+    if i in arr2 and i not in arr3:
+            arr3.append(i)
+arr3.sort()
+print(arr3)
